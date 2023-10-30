3bafb213c7f16c5bd140cd36c3953364d09040e6
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2023-10-30 22:32:17 +0300

    factories generating point charges, domain and subdomain are introduced

--- FMM_Project/Factory.h
+++ FMM_Project/Factory.h
@@ -1,67 +1,158 @@
 #pragma once
 #include <vector>
+#include <algorithm>
+#include <cassert>
+#include <tuple>
+#include <random>
+
 #include "Data.h"
 
-
-struct Factory
+/**
+ * @brief Descriptor of rectangular domain with point sources.
+ */
+struct Domain
 {
 	double x_a, x_b, y_a, y_b;
-	size_t m, q_per_block;
+	double width, height;
+	Domain(double x_a = 0.0, double x_b = 1.0, double y_a = 0.0, double y_b = 1.0) :
+		x_a{ x_a }, x_b{ x_b }, y_a{ y_a }, y_b{ y_b },
+		width{ x_b - x_a }, height{ y_b - y_a }
+	{}
+};
 
+/**
+ * @brief Factory that introduces a grid on the domain
+ * and generates the adjacency list of adjoining cells.
+ */
+struct AdjacencyFactory
+{
+	Domain domain;
+	size_t m;
+	// m x m grid within the domain
 	std::vector<double> x_grid, y_grid;
+	// descriptor of adkacent cells
+	// cell_ids --- contiguous cell ids adjacent to a cell I
+	// [cell_intervals[I]; cell_intervals[I+1]] --- contains the I-adjacent cells
+	std::vector<size_t> cell_ids, cell_intervals;
 
-	Factory(
-		size_t m, 
-		size_t q_per_block = 1ull, 
-		double x_a = 0.0, double x_b = 1.0, double y_a = 0.0, double y_b = 1.0) :
-		x_a{ x_a }, x_b{ x_b }, y_a{ y_a }, y_b{ y_b }, m{ m },
-		q_per_block{ q_per_block },
-		x_grid(m+1, 0.0),
-		y_grid(m+1, 0.0)
+	AdjacencyFactory(
+		size_t m,
+		const Domain& domain = Domain{}) :
+		domain{ domain }, m{ m },
+		x_grid(m + 1, 0.0),
+		y_grid(m + 1, 0.0)
 	{
-		double h_x{ (x_b - x_a) / m }, h_y{ (y_b - y_a) / m };
+		double hx{ domain.width / m }, hy{ domain.height / m };
 		for (size_t i = 0; i < m + 1; ++i)
 		{
-			x_grid[i] = x_a + i * h_x;
-			y_grid[i] = y_a + i * h_y;
+			x_grid[i] = domain.x_a + i * hx;
+			y_grid[i] = domain.y_a + i * hy;
 		}
+		set_adjacent_cells();
+	}
 
-		interval_ids.push_back(0ull);
-		for (size_t i = 0, l =0; i < m; ++i)
+	Domain sub_domain(size_t i, size_t j) const
+	{
+		assert(i < m - 1);
+		assert(j < m - 1);
+
+		return Domain{ x_grid[i] , x_grid[i+1] , y_grid[j] , y_grid[j+1] };
+	}
+
+protected:
+	void set_adjacent_cells()
+	{
+		cell_intervals.reserve(m * m + 1);
+		cell_ids.reserve(8 * m * m - 4 * (m + 1));
+
+		cell_intervals.push_back(0ull);
+		for (size_t j = 0, l = 0; j < m; ++j)
 		{
-			for (size_t j = 0; j < m; ++j, ++l)
+			for (size_t i = 0, count = 0; i < m; ++i, ++l)
 			{
+				if (i > 0)
+				{
+					++count;
+					cell_ids.push_back(l - 1ul);
+				}
+				if (i < m - 1ull)
+				{
+					++count;
+					cell_ids.push_back(l + 1ul);
+				}
+				if (j > 0)
+				{
+					++count;
+					cell_ids.push_back(l - m);
+				}
+				if (j < m - 1ull)
+				{
+					cell_ids.push_back(l + m);
+					++count;
+				}
+
+				if (i > 0 && j > 0)
+				{
+					++count;
+					cell_ids.push_back(l - 1ul - m);
+				}
+				if (i > 0 && j < m - 1ull)
+				{
+					++count;
+					cell_ids.push_back(l - 1ul + m);
+				}
+				if (i < m - 1ull && j > 0)
+				{
+					++count;
+					cell_ids.push_back(l - 1ul - m);
+				}
+				if (i < m - 1ull && j < m - 1ull)
+				{
+					++count;
+					cell_ids.push_back(l - 1ul + m);
+				}
+
+				std::sort(cell_ids.begin() + cell_intervals.back(), cell_ids.end());
+				cell_intervals.push_back(cell_intervals.back() + count);
+			}
+		}
+	}
+};
+
+/**
+ * @brief Generator of point charges within every subcell 
+ * of the main domain
+ */
+struct Factory
+{
+	size_t q_per_block;
+
+	Factory(
+		const AdjacencyFactory& grid,
+		size_t q_per_block = 1ull) :
+		q_per_block{ q_per_block },
+		rd{}
+	{
+		std::mt19937 gen(rd());
+		interval_ids.push_back(0ull);
+		for (size_t j = 0, l =0; j < grid.m; ++j)
+		{
+			for (size_t i = 0; i < grid.m; ++i, ++l)
+			{
+				// for every subdomain of the main domain
+				// generate a set of point charges
+				auto domain{ grid.sub_domain(i,j) };
+				std::uniform_real_distribution<> uniform_x(domain.x_a, domain.x_b);
+				std::uniform_real_distribution<> uniform_y(domain.y_a, domain.y_b);
 				for (size_t q_id = 0; q_id < q_per_block; ++q_id)
 				{
-				//	auto [x0, y0, q0] = generate_source(i, j);
-
-					//x.push_back(x0 /*x_grid[i] + h_x / 2.0*/);
-					//y.push_back(y0 /*y_grid[j] + h_y / 2.0*/);
-					//q.push_back(q0 /*1.0*/);
-
-					x.push_back(x_grid[i] + h_x / 2.0);
-					y.push_back(y_grid[j] + h_y / 2.0);
+					x.push_back(uniform_x(gen));
+					y.push_back(uniform_y(gen));
 					q.push_back(1.0);
 				}
 				interval_ids.push_back(interval_ids.back() + q_per_block);
-				neighbors.push_back(std::vector<size_t>{});
 			}
 		}
-
-		for (size_t l = 0; l < m * m; ++l)
-		{
-			size_t i{ l / m }, j{ l % m };
-			if (i == 0)
-			{// left-most column
-				neighbors[l].push_back();
-			}
-			if (i == m - 1)
-			{//right most column
-				neighbors[l].push_back();
-			}
-		}
-
-
 	}
 
 	auto get_sources() const
@@ -69,19 +160,10 @@ struct Factory
 		return SortedData{ x,y,q,interval_ids };
 	}
 
-	auto get_neighbor_list() const
-	{
-
-	}
-
 	std::vector<double> x, y, q;
 	std::vector<size_t> interval_ids;
-	std::vector<std::vector<size_t>> neighbors;
 
 private:
-	/*auto generate_source(size_t i, size_t j)
-	{
-		return Data{ random(x), random(y), random(q) };
-	}*/
-
+	// Seed with a real random value
+	std::random_device rd;
 };
